cdaabe2abd6e1b4a009f54bf9446d6831e376982
ChristopherHopper12 <christopherhopper2023@gmail.com>
2024-03-24 13:10:53 -0400

    GUI done

--- Password_Manager_SDEV265/Form1.cs
+++ Password_Manager_SDEV265/Form1.cs
@@ -16,5 +16,20 @@ namespace Password_Manager_SDEV265
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
