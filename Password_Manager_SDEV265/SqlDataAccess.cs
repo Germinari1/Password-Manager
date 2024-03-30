b25fb21835025150c65c49da565eec9da96e0f4d
Germinari1 <carreiralgc@gmail.com>
2024-03-30 11:49:35 -0400

    added comment to SqlDataAccess class and a related tutorial

--- Password_Manager_SDEV265/SqlDataAccess.cs
+++ Password_Manager_SDEV265/SqlDataAccess.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 
 namespace Password_Manager_SDEV265
 {
+    /* =-=-=-==-=-=-=-==-=-=-=-=-=-=-=--=-=-=-=-==-=-=-==-=-=-=-==-=-=-=-=-=-=-=--=-=-=-=-=
+    This class loads the connection stirng and will have (TODO) methods to laod and save data to our database.
+    This class is NOT completed (for more info and information on how to use it, see: https://www.youtube.com/watch?v=ayp3tHEkRc0 
+    =-=-=-==-=-=-=-==-=-=-=-=-=-=-=--=-=-=-=-==-=-=-==-=-=-=-==-=-=-=-=-=-=-=--=-=-=-=-=*/
+
     public class SqlDataAccess
     {
         private static string LoadConnectionString(string id = "Default")
